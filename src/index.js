a1adb78e6626589632c9cb197e0c7c738d3745c3
navinnavneet <navinnavneet810@gmail.com>
2026-05-10 18:02:01 +0530

    resolving cors issue

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://health-mate-prachi.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
